21317b07aaab16be05068b8200f719f6db604a30
shooore <shooore@gmail.com>
2022-09-08 18:05:25 +0300

    in progress

---
+++ site/coral.ru/js/main.coffee
@@ -0,0 +1,73 @@
+window.ASAP ||= (->
+    fns = []
+    callall = () ->
+        f() while f = fns.shift()
+    if document.addEventListener
+        document.addEventListener 'DOMContentLoaded', callall, false
+        window.addEventListener 'load', callall, false
+    else if document.attachEvent
+        document.attachEvent 'onreadystatechange', callall
+        window.attachEvent 'onload', callall
+    (fn) ->
+        fns.push fn
+        callall() if document.readyState is 'complete'
+)()
+
+window.log ||= () ->
+    if window.console and window.DEBUG
+        console.group? window.DEBUG
+        if arguments.length == 1 and Array.isArray(arguments[0]) and console.table
+            console.table.apply window, arguments
+        else
+            console.log.apply window, arguments
+        console.groupEnd?()
+window.trouble ||= () ->
+    if window.console
+        console.group? window.DEBUG if window.DEBUG
+        console.warn?.apply window, arguments
+        console.groupEnd?() if window.DEBUG
+
+window.preload ||= (what, fn) ->
+    what = [what] unless  Array.isArray(what)
+    $.when.apply($, ($.ajax(lib, dataType: 'script', cache: true) for lib in what)).done -> fn?()
+
+window.queryParam ||= queryParam = (p, nocase) ->
+    params_kv = location.search.substr(1).split('&')
+    params = {}
+    params_kv.forEach (kv) -> k_v = kv.split('='); params[k_v[0]] = k_v[1] or ''
+    if p
+        if nocase
+            return decodeURIComponent(params[k]) for k of params when k.toUpperCase() == p.toUpperCase()
+            return undefined
+        else
+            return decodeURIComponent params[p]
+    params
+
+window.DEBUG = 'APP NAME'
+
+ASAP ->
+
+    $('[data-popin]').on 'click', ->
+        $($(this).attr('data-popin')).addClass 'shown'
+
+    $('.popin .dismiss').on 'click', ->
+        $(this).closest('.shown').removeClass 'shown'
+
+    $('.programs .buttonlike').on 'click', ->
+        $this = $(this)
+        $this.addClass('yellow').siblings('.yellow').removeClass('yellow')
+        setTimeout ->
+            $('.flickity-enabled').flickity('resize')
+        , 10
+
+    preload 'https://cdnjs.cloudflare.com/ajax/libs/flickity/2.3.0/flickity.pkgd.min.js', ->
+        $('.opt-slider').flickity
+            cellSelector: '.slide'
+            cellAlight: 'left'
+            groupCells: yes
+            contain: yes
+            prevNextButtons: yes
+            pageDots: no
+        setTimeout ->
+            $('.flickity-enabled').flickity('resize')
+        , 100
---
+++ site/coral.ru/js/main.js
@@ -0,0 +1,135 @@
+var queryParam;
+
+window.ASAP || (window.ASAP = (function() {
+  var callall, fns;
+  fns = [];
+  callall = function() {
+    var f, results;
+    results = [];
+    while (f = fns.shift()) {
+      results.push(f());
+    }
+    return results;
+  };
+  if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', callall, false);
+    window.addEventListener('load', callall, false);
+  } else if (document.attachEvent) {
+    document.attachEvent('onreadystatechange', callall);
+    window.attachEvent('onload', callall);
+  }
+  return function(fn) {
+    fns.push(fn);
+    if (document.readyState === 'complete') {
+      return callall();
+    }
+  };
+})());
+
+window.log || (window.log = function() {
+  if (window.console && window.DEBUG) {
+    if (typeof console.group === "function") {
+      console.group(window.DEBUG);
+    }
+    if (arguments.length === 1 && Array.isArray(arguments[0]) && console.table) {
+      console.table.apply(window, arguments);
+    } else {
+      console.log.apply(window, arguments);
+    }
+    return typeof console.groupEnd === "function" ? console.groupEnd() : void 0;
+  }
+});
+
+window.trouble || (window.trouble = function() {
+  var ref;
+  if (window.console) {
+    if (window.DEBUG) {
+      if (typeof console.group === "function") {
+        console.group(window.DEBUG);
+      }
+    }
+    if ((ref = console.warn) != null) {
+      ref.apply(window, arguments);
+    }
+    if (window.DEBUG) {
+      return typeof console.groupEnd === "function" ? console.groupEnd() : void 0;
+    }
+  }
+});
+
+window.preload || (window.preload = function(what, fn) {
+  var lib;
+  if (!Array.isArray(what)) {
+    what = [what];
+  }
+  return $.when.apply($, (function() {
+    var i, len, results;
+    results = [];
+    for (i = 0, len = what.length; i < len; i++) {
+      lib = what[i];
+      results.push($.ajax(lib, {
+        dataType: 'script',
+        cache: true
+      }));
+    }
+    return results;
+  })()).done(function() {
+    return typeof fn === "function" ? fn() : void 0;
+  });
+});
+
+window.queryParam || (window.queryParam = queryParam = function(p, nocase) {
+  var k, params, params_kv;
+  params_kv = location.search.substr(1).split('&');
+  params = {};
+  params_kv.forEach(function(kv) {
+    var k_v;
+    k_v = kv.split('=');
+    return params[k_v[0]] = k_v[1] || '';
+  });
+  if (p) {
+    if (nocase) {
+      for (k in params) {
+        if (k.toUpperCase() === p.toUpperCase()) {
+          return decodeURIComponent(params[k]);
+        }
+      }
+      return void 0;
+    } else {
+      return decodeURIComponent(params[p]);
+    }
+  }
+  return params;
+});
+
+window.DEBUG = 'APP NAME';
+
+ASAP(function() {
+  $('[data-popin]').on('click', function() {
+    return $($(this).attr('data-popin')).addClass('shown');
+  });
+  $('.popin .dismiss').on('click', function() {
+    return $(this).closest('.shown').removeClass('shown');
+  });
+  $('.programs .buttonlike').on('click', function() {
+    var $this;
+    $this = $(this);
+    $this.addClass('yellow').siblings('.yellow').removeClass('yellow');
+    return setTimeout(function() {
+      return $('.flickity-enabled').flickity('resize');
+    }, 10);
+  });
+  return preload('https://cdnjs.cloudflare.com/ajax/libs/flickity/2.3.0/flickity.pkgd.min.js', function() {
+    $('.opt-slider').flickity({
+      cellSelector: '.slide',
+      cellAlight: 'left',
+      groupCells: true,
+      contain: true,
+      prevNextButtons: true,
+      pageDots: false
+    });
+    return setTimeout(function() {
+      return $('.flickity-enabled').flickity('resize');
+    }, 100);
+  });
+});
